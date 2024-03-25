9313c6236c1de265ecd14eb1658785815f6eebc2
David <Muchahi 853@gmail.com>
2024-03-25 19:48:25 +0300

    Finished lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1=31;
+const num2=2;
+const multiply=num1*num2;
+const random=Math.floor(Math.random()*10)+1;
+console.log(random);
+const num3=8;
+const num4=2;
+const mod=num3/num4;
+console.log(mod);
+const max=Math.max(4,6,10,2,15,20);
+console.log(max);
